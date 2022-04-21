3fcaabaafc48a515dbd3f57ae783da09c8fd47af
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-04-21 10:30:26 +0530

    Created Test Case- 1.22 to check addition of 1Feet and 2inch should return 14inch

--- src/test/java/quantity_measurementTDD/Testting.java
+++ src/test/java/quantity_measurementTDD/Testting.java
@@ -7,16 +7,28 @@ import quantityMeasurementTDD.UnitMeasurementSystem;
 
 public class Testting {
 
-
+    public static UnitMeasurementSystem length1;
+    public static UnitMeasurementSystem length2;
+    public static double check;
     /*
     Test Case -1.22 (2Inch+2Inch = 4Inch)
      */
     @Test
     public void given2InchAnd2Inch_ShouldReturn4Inch(){
-        UnitMeasurementSystem length1= new UnitMeasurementSystem(Length.INCH,2.0);
-        UnitMeasurementSystem length2 = new UnitMeasurementSystem(Length.INCH,2.0);
-        double check = length1.add(length2);
+        length1= new UnitMeasurementSystem(Length.INCH,2.0);
+        length2 = new UnitMeasurementSystem(Length.INCH,2.0);
+        check = length1.add(length2);
         Assert.assertEquals(4.0,check,0.0);
     }
 
+    /*
+    Test Case -1.23 (1Feet + 2Inch = 14Inch)
+     */
+    @Test
+    public void given1FeetAnd2Inch_ShouldReturn14Inch(){
+        length1 = new UnitMeasurementSystem(Length.FEET,1.0);
+        length2 = new UnitMeasurementSystem(Length.INCH,2.0);
+        check = length1.add(length2);
+        Assert.assertEquals(14,check,0.0);
+    }
 }
